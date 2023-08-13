0cc8ed3dd96bf0515cc2118d581b6e6770b8dcce
Mewes Kochheim <kochheim@develop4edu.de>
2023-08-13 20:10:43 +0200

    Node error handling

--- src/main/loader.ts
+++ src/main/loader.ts
@@ -7,30 +7,34 @@ import hygData35Csv from '../../resources/hygdata_v35.csv?asset';
 
 export default function (window: BrowserWindow): Promise<void> {
   return new Promise<void>((resolve, reject) => {
-    const parser = parse({
-      columns: true,
-      delimiter: ',',
-      skip_empty_lines: true,
-      trim: true
-    })
-      .on('readable', () => {
-        const buffer = [] as Star[];
-        let star: Star;
-        while ((star = parser.read()) !== null) {
-          buffer.push(star);
-        }
-        if (buffer.length > 0) {
-          window.webContents.send('update', buffer);
-        }
+    try {
+      const parser = parse({
+        columns: true,
+        delimiter: ',',
+        skip_empty_lines: true,
+        trim: true
       })
-      .on('error', (e) => reject(e))
-      .on('end', () => {
-        window.webContents.send('update', []);
-        resolve();
-      });
+        .on('readable', () => {
+          const buffer = [] as Star[];
+          let star: Star;
+          while ((star = parser.read()) !== null) {
+            buffer.push(star);
+          }
+          if (buffer.length > 0) {
+            window.webContents.send('update', buffer);
+          }
+        })
+        .on('error', (e) => reject(e))
+        .on('end', () => {
+          window.webContents.send('update', []);
+          resolve();
+        });
 
-    fs.createReadStream(hygData35Csv)
-      .on('data', (data) => parser.write(data.toString()))
-      .on('end', () => parser.end());
+      fs.createReadStream(hygData35Csv)
+        .on('data', (data) => parser.write(data.toString()))
+        .on('end', () => parser.end());
+    } catch (e) {
+      reject(e);
+    }
   });
 }
