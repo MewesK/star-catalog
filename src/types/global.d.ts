66926be4ea11e6a1746b8611c8517cd449e3bd36
Mewes Kochheim <kochheim@develop4edu.de>
2023-08-16 23:16:16 +0200

    TS stuff

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,7 +1,20 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { FlyControls } from 'three/examples/jsm/controls/FlyControls';
 declare module 'three/examples/jsm/controls/FlyControls' {
   interface FlyControls {
-    status: number;
-    pointerup: (event: PointerEvent) => void;
+    moveState: {
+      up: 0;
+      down: 0;
+      left: 0;
+      right: 0;
+      forward: 0;
+      back: 0;
+      pitchUp: 0;
+      pitchDown: 0;
+      yawLeft: 0;
+      yawRight: 0;
+      rollLeft: 0;
+      rollRight: 0;
+    };
   }
 }
