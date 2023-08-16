33b53cd4c8e05a9e5f07f8997b2e24ef2a18ef88
Mewes Kochheim <kochheim@develop4edu.de>
2023-08-16 23:14:38 +0200

    Fixed browser select

--- src/renderer/src/components/StarBrowser.vue
+++ src/renderer/src/components/StarBrowser.vue
@@ -10,19 +10,14 @@ const { height } = useElementSize(parentEl);
 
 <template>
   <v-virtual-scroll :height="height" item-height="48" :items="starsInRange">
-    <template #default="{ item }">
+    <template #default="{ item, index }">
       <v-list-item
         :title="getStarName(item)"
         :subtitle="`(${format(item.x)}, ${format(item.y)}, ${format(item.z)})`"
         :base-color="item.proper || item.bf || item.gl ? '' : 'grey-darken-1'"
       >
         <template #append>
-          <v-btn
-            icon="navigate_next"
-            size="xx-small"
-            variant="tonal"
-            @click="selectStar(item.id)"
-          />
+          <v-btn icon="navigate_next" size="xx-small" variant="tonal" @click="selectStar(index)" />
         </template>
       </v-list-item>
     </template>
