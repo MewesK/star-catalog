bd4068a429901789ee95273f072aaf24adc6ec1d
MewesK <mewes@kochheim.de>
2023-08-20 18:50:43 +0200

    Fixed unused StaticStarMaterial

--- src/renderer/src/three/textures.ts
+++ src/renderer/src/three/textures.ts
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 
 import sunTextureImage from '../assets/2k_sun_alpha.png';
+import sunBwTextureImage from '../assets/2k_sun_bw.png';
 import pointTextureImage from '../assets/particle_light.png';
 import surfaceTextureImage from '../assets/sun_surface_nso_recreated1.png';
 
@@ -15,5 +16,8 @@ surfaceTexture.wrapS = surfaceTexture.wrapT = THREE.RepeatWrapping;
 
 export const sunTexture = load(sunTextureImage);
 sunTexture.wrapS = sunTexture.wrapT = THREE.RepeatWrapping;
+
+export const sunBwTexture = load(sunBwTextureImage);
+sunBwTexture.wrapS = sunBwTexture.wrapT = THREE.RepeatWrapping;
 
 export const pointTexture = load(pointTextureImage);
